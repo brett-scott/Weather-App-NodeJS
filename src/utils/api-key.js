03d16f7dbc125e047a9f305cba01f5ea62097cf9
Brett <39827151+brett-scott@users.noreply.github.com>
2020-12-03 09:20:32 +1000

    Removed private API key, added an API key setup file

---
+++ src/utils/api-key.js
@@ -0,0 +1,4 @@
+//  Head to https://weatherstack.com/ and create an account to get your key
+const api_key = 'KEY_HERE';
+
+module.exports = api_key;
--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,12 +1,17 @@
 const request = require('request');
+const api_key = require('./api-key.js');
 
 const forecast = (lat, long, unit, callback) => {
-    const url = `http://api.weatherstack.com/current?access_key=28c0da2f6eb7c299937d75544c9265e0&query=${encodeURIComponent(lat)},${encodeURIComponent(long)}&units=${unit}`
+    const url = `http://api.weatherstack.com/current?access_key=${api_key}&query=${encodeURIComponent(lat)},${encodeURIComponent(long)}&units=${unit}`
     
     request({url, json: true}, (error, { body }) => {
         if(error){
             callback('Unable to connect to weather service', undefined);
         } else if(body.error){
+            if(body.error.type === 'invalid_access_key'){
+                return callback("You have provided an invalid API key (src/utils/api-key.js)", undefined);
+            }
+            console.log(body.error)
             callback('Unable to find location', undefined);
         } else {
             callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature}(${unit === 'm' ? 'C' : 'F'}) degrees, it feels like ${body.current.feelslike}(${unit === 'm' ? 'C' : 'F'}) degrees. The humidity is ${body.current.humidity}%.`);
